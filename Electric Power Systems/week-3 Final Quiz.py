1a03e45d7b78a93b1fdc110f44b233919d36eeb1
haneefnani <55850551+haneefnani@users.noreply.github.com>
2020-04-18 07:56:21 +0530

    Create week-3 Final Quiz.py

---
+++ Electric Power Systems/week-3 Final Quiz.py
@@ -0,0 +1,129 @@
+1.Which of the following is not a switch gear component?
+
+
+Busbar
+
+
+Grounding Breaker
+
+
+Circuit Breaker
+
+
+Surge arresters
+
+
+Switches
+
+2.Fill in the blank with the correct answer
+
+A ______ serves as a network node in physical terms and constitutes the start and end of individual lines.
+
+Enter answer here
+_____________________________
+
+
+
+3.How can a circuit breaker be reset?
+
+
+Manually
+
+
+Automatically
+
+
+All of the above
+
+
+None of the above
+
+4.What do disconnect switches lack compared to circuit breakers?
+
+
+Lockout
+
+
+Mechanism for electric arc suppresion
+
+
+Lockable overlads
+
+
+Transformers
+
+
+
+5.Disconnect switches can trip open when it detects an over current situation.
+
+
+True
+
+
+False
+
+
+6.When can switches be operated?
+
+
+Only in an energized state
+
+
+Only in an arcing state
+
+
+Only in a de-energized state
+
+
+Only in a live state
+
+
+7.Each Switch gear system and substation is divided into individual fields. Which option below is not one of the individual fields?
+
+
+Current Transformer
+
+
+Generator
+
+
+Busbar
+
+
+Circuit breaker
+
+1 point
+8.Question 8
+The purpose of a grounding switch is to _______ the circuit breaker and provide a mechanism to automatically close the breaker.
+
+Enter answer here
+
+
+9.In relation to ensuring the reliability of electrical service, how is redundancy achieved?
+
+By arranging switches into several sections.
+
+
+By arranging Busbars into several sections.
+
+
+By arranging circuits in several sections.
+
+
+By arranging transformers into several sections.
+
+
+10.What is the highest transmission voltage that high-voltage switches can handle?
+
+
+1M Volts
+
+
+2M Volts
+
+
+2.5M Volts
+
+
+3M Volts
+
