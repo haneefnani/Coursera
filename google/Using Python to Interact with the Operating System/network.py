9889b1182c06faa6d936bab068aeb219c6519d83
haneefnani <55850551+haneefnani@users.noreply.github.com>
2020-05-15 21:44:18 +0530

    Create network.py

---
+++ google/Using Python to Interact with the Operating System/network.py
@@ -0,0 +1,18 @@
+import requests
+import socket
+
+def check_localhost():
+    localhost = socket.gethostbyname('localhost')
+    return localhost
+def check_connectivity():
+    request = requests.get("http://www.google.com")
+    return request
+#!/usr/bin/env python3
+import requests
+import socket
+
+def check_localhost():
+    localhost = socket.gethostbyname('localhost')
+    return localhost
+def check_connectivity():
+    request = requests.get("http://www.google.com")
